8e686e749c28b93084ab2b12e4ca3ef7967bb790
LueYangYue <a202211@siswa.ukm.edu.my>
2025-12-26 22:35:46 +0800

    Edit task notification UI

--- cleaner_dashboard.php
+++ cleaner_dashboard.php
@@ -820,6 +820,9 @@ $todayTasks = $conn->query("SELECT COUNT(*) as count FROM task WHERE staffID='$c
                         <button class="btn btn-outline" onclick="markAllAsRead()">
                             <i class="fas fa-check-double"></i> Mark All Read
                         </button>
+                        <button class="btn btn-outline" onClick="viewAll()">
+                            <i class="far fa-plus-square"></i> View All
+                        </button>
                     </div>
                 </div>
                 
@@ -1026,6 +1029,13 @@ $todayTasks = $conn->query("SELECT COUNT(*) as count FROM task WHERE staffID='$c
     // Mark all notifications as read
     function markAllAsRead(){
         $.post('mark_all_read.php', {userID: '<?= $cleanerID ?>'}, function(){
+            loadNotifications();
+        });
+    }
+
+    // View All notifications
+    function viewAll(){
+        $.post('viewAll.php', {user: '<?=$cleanerID?>'}, function(){
             loadNotifications();
         });
     }
